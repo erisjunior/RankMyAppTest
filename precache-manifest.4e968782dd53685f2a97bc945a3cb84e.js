4a756f6fd4e181a4586694082bdcc6a386ffe972
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-10-25 01:17:14 -0300

    Updates

---
+++ precache-manifest.4e968782dd53685f2a97bc945a3cb84e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9477c97ff9e3e9c8478993ccf273ca4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d2a4bc63161f3beb1ac",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d008f69ab2f54bd60d1d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7d2a4bc63161f3beb1ac",
+    "url": "/static/js/2.47c2b262.chunk.js"
+  },
+  {
+    "revision": "d008f69ab2f54bd60d1d",
+    "url": "/static/js/main.4e57b3e7.chunk.js"
+  },
+  {
+    "revision": "64371b16a571b6709272",
+    "url": "/static/js/runtime-main.fe1321a2.js"
+  }
+]);
